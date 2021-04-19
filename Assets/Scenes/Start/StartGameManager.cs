74fafb16b4c19ce1b6930d8d56a06ba3dbe48347
MAPLELEAF3659 <ig910425@gmail.com>
2021-04-19 12:25:40 +0800

    Alpha 0.0.1
    Testing AR image tracking /face tracking.
    Add scene: start,selectClan,cloth,boat,house
    Add asset: Cartoon FX Free

---
+++ Assets/Scenes/Start/StartGameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGameManager : MonoBehaviour
+{
+    public Button clanBtn, clothBtn;
+    public Text version;
+    public void clanBtnOnClick()
+    {
+        SceneManager.LoadScene("SelectClan");
+    }
+    public void clothBtnOnClick()
+    {
+        SceneManager.LoadScene("Cloth");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        version.text = "Ver " + Application.version;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
